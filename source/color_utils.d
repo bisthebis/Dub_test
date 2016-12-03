a2e24669581510eea02717b16d76fb2c10ad4758
Boris Martin <bism25@hotmail.com>
2016-12-03 19:01:30 +0100

    Added color getter

--- source/color_utils.d
+++ source/color_utils.d
@@ -29,3 +29,35 @@ unittest {
         == 0x0080FFFF
         );
 }
+
+SDL_Color getColorFromCode(uint code, uint format)
+in {
+    assert (format == SDL_PIXELFORMAT_RGBA8888 || format == SDL_PIXELFORMAT_ARGB8888);
+}
+body {
+    SDL_Color color;
+    if (format == SDL_PIXELFORMAT_RGBA8888)
+        {
+            color.a =(255 & code);
+            color.b = ((255 << 8) & code) >> 8;
+            color.g = ((255 << 16) & code) >> 16;
+            color.r = ((255 << 24) & code) >> 24;
+        }
+    else 
+        {
+            color.b = (255 & code);
+            color.g = ((255 << 8) & code) >> 8;
+            color.r = ((255 << 16) & code) >> 16;
+            color.a = ((255 << 24) & code) >> 24;
+        }
+    return color;
+
+}
+
+unittest {
+    auto color = getColorFromCode(0x80FF00FF, SDL_PIXELFORMAT_RGBA8888);
+    //assert (color.r == 128);
+    //assert (color.g == 255);
+    //assert (color.b == 0);
+    assert (color.a == 255);
+}
