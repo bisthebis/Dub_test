ec79e558943befe8c7d1dcdbf1839dfc78f53b66
Boris Martin <bism25@hotmail.com>
2016-12-03 18:30:34 +0100

    added Bitmap texture

---
+++ source/bitmap_texture.d
@@ -0,0 +1,12 @@
+import derelict.sdl2.sdl;
+
+class BitmapTexture 
+{
+    const uint width;
+    const uint height;
+
+    this (uint w, uint h) {
+        width = w;
+        height = h;
+    }
+}
