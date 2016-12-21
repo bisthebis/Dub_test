ca9fc35b6cc58aad76b55f605cdd685b3e19ef57
Boris Martin <bism25@hotmail.com>
2016-12-21 22:43:08 +0100

    Made public functions at top of the file

--- source/sierpinski_carpet.d
+++ source/sierpinski_carpet.d
@@ -1,6 +1,25 @@
 import bitmap;
 import color_utils;
 
+void sierpinskiDo(ref Bitmap bitmap, uint size, uint steps)
+in {
+    assert (steps > 0);
+}
+body {
+    sierpinskiPrepare(bitmap, size);
+    sierpinskiStep(bitmap, 0, 0, size);
+    if (steps > 1)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                sierpinskiStep(bitmap, 0 + i*size / 3, 0 + j*size / 3, size/3);
+            }
+        }
+    }
+}
+
 private bool isPowerOfThree(uint x)
 {
     if (x == 0 || x == 3)
@@ -30,26 +49,6 @@ body {
         for (uint x = 0; x < size; ++x)
         {
             bitmap.setColor(x, y, getColorCode(255, 255, 255, 255));
-        }
-    }
-}
-
-
-void sierpinskiDo(ref Bitmap bitmap, uint size, uint steps)
-in {
-    assert (steps > 0);
-}
-body {
-    sierpinskiPrepare(bitmap, size);
-    sierpinskiStep(bitmap, 0, 0, size);
-    if (steps > 1)
-    {
-        for (int j = 0; j < 3; ++j)
-        {
-            for (int i = 0; i < 3; ++i)
-            {
-                sierpinskiStep(bitmap, 0 + i*size / 3, 0 + j*size / 3, size/3);
-            }
         }
     }
 }
